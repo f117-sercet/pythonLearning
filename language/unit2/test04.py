d3e71646cdd6886d7eea98a1e90bc689c19cbdea
sowrd <15159885771@163.com>
2023-02-22 18:47:26 +0800

    字典

--- language/unit2/test04.py
+++ language/unit2/test04.py
@@ -1,1 +1,13 @@
 # 字典遍历
+# 利用item遍历所有值
+d1 = {"Tom": 10, "JIM": 5, "mike": 11, "jack": 12}
+
+for l in d1.items():
+    print(l)
+
+# 遍历所有键:
+for get in d1:
+    print(get)
+    print(d1[get])
+    print(d1.values())
+
