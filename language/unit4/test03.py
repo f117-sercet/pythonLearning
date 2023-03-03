17bbfb6761eba15155fd6b54f9a907a46dc61841
sowrd <15159885771@163.com>
2023-03-03 19:23:18 +0800

    函数编写

--- language/unit4/test03.py
+++ language/unit4/test03.py
@@ -15,10 +15,13 @@ watermelon('西瓜', '甜', '圆形', '绿色')
 
 #### 传递任意数量的键值对
 def watermelons(name, **attributes):
-
-
     print(name)
     print(type(attributes))
     return attributes
 
     print(watermelons('西瓜', taste='甜', shape='原型'))
+
+#  函数传递对象总结
+# 不可变对象：在函数里进行修改数值，会变成新的对象
+# 可变对象，在函数里进行值修改，函数内外还是同一个对象，但是值同步发生变化
+
---
+++ language/unit5/test1.py
@@ -0,0 +1,5 @@
+# 类
+class box1:
+
+    def _init_(self, length1, width1, height1):
+        self.length = length1
