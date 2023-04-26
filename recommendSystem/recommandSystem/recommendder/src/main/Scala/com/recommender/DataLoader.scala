2a9e208cd10a3edaf4abecdebd71580b4e0dd3c0
sowrd <15159885771@163.com>
2023-04-26 16:56:05 +0800

    scala编写,日志文件编写

--- recommendSystem/recommandSystem/recommendder/src/main/Scala/com/recommender/DataLoader.scala
+++ recommendSystem/recommandSystem/recommendder/src/main/Scala/com/recommender/DataLoader.scala
@@ -1,4 +1,5 @@
-
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.SparkSession
 
 /**
  *
@@ -34,3 +35,77 @@ case class Movie(mid: Int, name: String, descri: String, timelong: String, issue
  */
 
 case class Rating(uid:Int,mid:Int,score:Double,timestamp:Int)
+
+/**
+ * Tag数据集
+ *
+ * 15,1955,dentist,1193435061
+ */
+case class Tag(uid: Int, mid: Int, tag: String, timestamp: Int)
+
+// 把mongo和es的配置封装成样例类
+
+/**
+ *
+ * @param uri MongoDB连接
+ * @param db  MongoDB数据库
+ */
+case class MongoConfig(uri:String, db:String)
+
+/**
+ *
+ * @param httpHosts       http主机列表，逗号分隔
+ * @param transportHosts  transport主机列表
+ * @param index            需要操作的索引
+ * @param clustername      集群名称，默认elasticsearch
+ */
+case class ESConfig(httpHosts:String, transportHosts:String, index:String, clustername:String)
+
+object DataLoader{
+  // 定义常量
+  val MOVIE_DATA_PATH = "F:\\git\\Repository\\gitHub\\pythonLearning\\recommendSystem\\recommandSystem\\recommendder\\src\\main\\resources\\movies.csv"
+  val RATING_DATA_PATH = "F:\\git\\Repository\\gitHub\\pythonLearning\\recommendSystem\\recommandSystem\\recommendder\\src\\main\\resources\\ratings.csv"
+  val TAG_DATA_PATH = "F:\\git\\Repository\\gitHub\\pythonLearning\\recommendSystem\\recommandSystem\\recommendder\\src\\main\\resources\\tags.csv"
+
+  val MONGODB_MOVIE_COLLECTION = "Movie"
+  val MONGODB_RATING_COLLECTION = "Rating"
+  val MONGODB_TAG_COLLECTION = "Tag"
+  val ES_MOVIE_INDEX = "Movie"
+
+  def main(args: Array[String]): Unit = {
+    val config = Map(
+      "spark.cores" -> "local[*]",
+      "mongo.uri" -> "mongodb://localhost:27017/recommender",
+      "mongo.db" -> "recommender",
+      "es.httpHosts" -> "localhost:9200",
+      "es.transportHosts" -> "localhost:9300",
+      "es.index" -> "recommender",
+      "es.cluster.name" -> "elasticsearch")
+
+  // 创建一个sparkConf
+
+  //def config(str: String): String = ???
+
+    val sparkConf = new SparkConf().setMaster(config("spark.cores")).setAppName("DataLoader")
+
+    // 创建一个SparkSession
+    val spark = SparkSession.builder().config(sparkConf).getOrCreate()
+    import spark.implicits._
+    // 加载数据
+    val movieRDD = spark.sparkContext.textFile(MOVIE_DATA_PATH)
+
+    val movieDF = movieRDD.map(
+      item => {
+        val attr = item.split("\\^")
+        Movie(attr(0).toInt, attr(1).trim, attr(2).trim, attr(3).trim, attr(4).trim, attr(5).trim, attr(6).trim, attr(7).trim, attr(8).trim, attr(9).trim)
+      }
+    ).toDF()
+
+    val ratingRDD = spark.sparkContext.textFile(RATING_DATA_PATH)
+
+    ratingRDD.map(item =>{
+      val attr = item.split(",")
+      Rating(attr(0).toInt, attr(1).toInt, attr(2).toInt, attr(3).toInt))
+  }).toDF()
+
+}
