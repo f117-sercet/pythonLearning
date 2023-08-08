9c0033a3655414cb6468bb08b20f002fdf5ed44f
sowrd <15159885771@163.com>
2023-08-08 15:47:07 +0800

    tensorflow 基本概念

---
+++ tensorflow/concenpt/test02.py
@@ -0,0 +1,18 @@
+import tensorflow as tf
+
+
+# 张量——不同维度的数组
+# 张量只是引用了程序中的运算结果而不是一个真正的数组。
+# 张量保存的是运算结果的属性，而不是真正的数字。
+
+def test():
+    a = tf.constant([1.0, 2.0], name="a")
+    b = tf.constant([3.0, 4.0], name="b")
+
+    result = a + b
+    print(result)
+
+
+if __name__ == '__main__':
+
+    test()
