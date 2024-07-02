a4bfa6cfbd121d78bdb419443a2a099ffb6350c3
glowworm <15159885771@163.com>
2024-07-02 17:08:28 +0800

    重新修改

---
+++ recommendSystem/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,11 @@
+
+d
+4MovieRecommendSystem/recommender/KafkaStream/pom.xml,0\3\033e2213bf3c78b7b96aa92410a073ae738b6c41
+£
+sMovieRecommendSystem/recommender/ContentRecommender/src/main/scala/com/recommender/content/ContentRecommender.scala,2\b\2b7d9603d1f525e3875311cbcaa6424c8c8380e2
+”
+dMovieRecommendSystem/recommender/KafkaStream/src/main/java/com/atguigu/kafkastream/LogProcessor.java,0\a\0af3150c444202918c0aa7abc926c20cd0359757
+“
+cMovieRecommendSystem/recommender/KafkaStream/src/main/java/com/atguigu/kafkastream/Application.java,0\5\05a216ea8787c66103449898fe8a70170049cdd9
+‹
+[MovieRecommendSystem/recommender/DataLoader/src/main/scala/com/recommender/DataLoader.scala,e\1\e17fe46b0f673f048a8dbe2d0125582932122558
--- tensorflow/mnistDataSet/test02.py
+++ tensorflow/mnistDataSet/test02.py
@@ -2,7 +2,8 @@ import tensorflow as tf
 
 from tensorflow.core.example.tutorials.mnist import input_data
 
-#tf.disable_v2_behavior()
+
+# tf.disable_v2_behavior()
 
 
 def hidden_layer(input_tensor, weight1, bias1, weight2, bias2, lay_name):
@@ -16,10 +17,11 @@ def hidden_layer(input_tensor, weight1, bias1, weight2, bias2, lay_name):
     # 最大训练步数
     max_steps = 3000
 
-    training_steps = tf.Variable(0,trainable=False)
+    training_steps = tf.Variable(0, trainable=False)
 
     layer1 = tf.nn.relu(tf.matmul(input_tensor, weight1) + bias1)
     return tf.matmul(layer1, weight2) + bias2
+
 
 if __name__ == '__main__':
     tf.compat.v1.disable_eager_execution()
@@ -27,14 +29,13 @@ if __name__ == '__main__':
     y_ = tf.compat.v1.placeholder(tf.float32, [None, 10], name="y-output")
 
     # 生成隐藏层参数
-    weight1 = tf.compat.v1.Variable(tf.compat.v1.truncated_normal([784,500],stddev=0.1))
-    biase1 = tf.compat.v1.Variable(tf.compat.v1.constant(0.1,shape=[500]))
+    weight1 = tf.compat.v1.Variable(tf.compat.v1.truncated_normal([784, 500], stddev=0.1))
+    biase1 = tf.compat.v1.Variable(tf.compat.v1.constant(0.1, shape=[500]))
 
     # 生成输出层参数，其中weight2包含500个
-    weight2 = tf.compat.v1.Variable(tf.compat.v1.truncated_normal([500,10],stddev=0.1))
-    biase2 = tf.compat.v1.Variable(tf.compat.v1.constant(0.1,shape=[10]))
+    weight2 = tf.compat.v1.Variable(tf.compat.v1.truncated_normal([500, 10], stddev=0.1))
+    biase2 = tf.compat.v1.Variable(tf.compat.v1.constant(0.1, shape=[10]))
     # 计算得到的y值
-    y = hidden_layer(x, weight1, biase1, weight2,biase2,"y")
+    y = hidden_layer(x, weight1, biase1, weight2, biase2, "y")
     # test
     print(y)
-    #### 123
