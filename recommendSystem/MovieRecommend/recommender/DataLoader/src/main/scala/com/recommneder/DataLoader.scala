3cacf74a60793d32d1a57da6c3c9009122fee7e3
glowworm <15159885771@163.com>
2024-07-08 15:47:04 +0800

    Mongo数据库连接配置

--- recommendSystem/MovieRecommend/recommender/DataLoader/src/main/scala/com/recommneder/DataLoader.scala
+++ recommendSystem/MovieRecommend/recommender/DataLoader/src/main/scala/com/recommneder/DataLoader.scala
@@ -1,6 +1,7 @@
 package com.recommneder
 
 import com.mongodb.casbah.MongoClient
+import com.mongodb.casbah.commons.MongoDBObject
 import com.recommneder.DataLoader.storeDataInMongoDB
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.{DataFrame, SparkSession}
@@ -31,17 +32,18 @@ case class Tag(uid:Int,
                tag:String,
                timeStamp:Long)
 
-case class MongoDBConf(uri:String,db:String)
-case class ESConf(
-                   httpHosts:String,
-                   transportHosts:String,
-                   index:String,
-                   clustername:String)
+
+case class MongoConfig(uri:String, db:String)
+
+case class ESConfig(httpHosts:String, transportHosts:String, index:String,
+
+                    clustername:String)
+
 object DataLoader {
 
 
     val MOVIE_DATA_PATH ="F:\\git\\Repository\\gitHub\\pythonLearning\\recommendSystem\\MovieRecommend\\recommender\\DataLoader\\src\\main\\resources\\movies.csv"
-    val RATING_DATA_PATH ="F:\\git\\Repository\\gitHub\\pythonLearning\\recommendSystem\\MovieRecommend\\recommender\\DataLoader\\src\\main\\resources\\rating.csv"
+    val RATING_DATA_PATH ="F:\\git\\Repository\\gitHub\\pythonLearning\\recommendSystem\\MovieRecommend\\recommender\\DataLoader\\src\\main\\resources\\ratings.csv"
     val TAG_DATA_PATH ="F:\\git\\Repository\\gitHub\\pythonLearning\\recommendSystem\\MovieRecommend\\recommender\\DataLoader\\src\\main\\resources\\tags.csv"
 
   val MONGODB_MOVIE_COLLECTION="Movie"
@@ -52,14 +54,14 @@ object DataLoader {
 
 
     val config = Map("spark.cores"-> "local[*]",
-      "mongo.uri"->"mongodb://influxdb:27017/recommender",
-    "mongo.db"->"recommender",
-    "es.httpHosts"->"localhost:9200",
-    "es.transportHosts"->"localhost:9300",
-    "es:index"->"recommender",
-    "es.cluster.name"->"elasticsearch")
+      "mongo.uri"->"mongodb://192.168.10.105:27017/recommender",
+      "mongo.db"->"recommender",
+      "es.httpHosts"->"localhost:9200",
+      "es.transportHosts"->"localhost:9300",
+      "es:index"->"recommender",
+      "es.cluster.name"->"elasticsearch")
     // 创建sparkConfig对象
-    val sparkConf = new SparkConf().setMaster(config("saprk.cores")).setAppName("DataLoader")
+    val sparkConf = new SparkConf().setMaster(config("spark.cores")).setAppName("DataLoader")
 
     // 创建sparkSession
     val spark = SparkSession.builder().config(sparkConf)getOrCreate()
@@ -73,43 +75,75 @@ object DataLoader {
     val movieRDD = spark.sparkContext.textFile(MOVIE_DATA_PATH)
     val movieDF = movieRDD.map(item =>{
       val attr = item.split("\\^")
-      Movie(attr(0).toInt, attr(1).trim, attr(2).trim, attr(3).trim, attr(4).trim, attr(5).trim, attr(6).trim, attr(7).trim, attr(8).trim, attr(9).trim)
-    }
-    ).toDF()
+      Movie(attr(0).trim.toInt,attr(1).trim,attr(2).trim,attr(3).trim,attr(4).trim, attr(5).trim,attr(6).trim,attr(7).trim,attr(8).trim,attr(9).trim)
+    }).toDF()
+
 
     //rating转换为 DF
     val ratingRDD = spark.sparkContext.textFile(RATING_DATA_PATH)
     val ratingDF = ratingRDD.map(item => {
       val attr = item.split(",")
-      Rating(attr(0).toInt, attr(1).toInt, attr(2).toDouble, attr(3).toInt)
+      Rating(attr(0).trim.toInt, attr(1).trim.toInt, attr(2).trim.toDouble, attr(3).trim.toInt)
     }).toDF()
 
     val tagRDD = spark.sparkContext.textFile(TAG_DATA_PATH)
     val tagDF  = tagRDD.map(item=>{
       val attr = item.split(",")
-      Tag(attr(0).toInt, attr(1).toInt, attr(2).trim, attr(3).toInt)
+      Tag(attr(0).trim.toInt, attr(1).trim.toInt, attr(2).trim, attr(3).trim.toInt)
     }).toDF()
 
-  implicit val mongoConfig= MongoDBConf(config("mongo.uri"),config("mongo.db"))
+  implicit val mongoConfig= MongoConfig(config.get("mongo.uri").get,config.get("mongo.db").get)
 
     // 将数据保存到MongoDB
     storeDataInMongoDB(movieDF,ratingDF,tagDF);
 
     // 数据预处理
     // 保存到ES
-    storeDataInES();
-
+    //storeDataInES();
     spark.stop()
   }
 
-  private def storeDataInMongoDB(movieDF:DataFrame,ratingDF:DataFrame,tagDF:DataFrame)(implicit mongoConfig:MongoDBConf) = {
+  private def storeDataInMongoDB(movieDF:DataFrame,ratingDF:DataFrame,tagDF:DataFrame)(implicit mongoConfig:MongoConfig) = {
 
     //新建MongoDB连接
-    val mongoClient = MongoClient(mongoConfig.uri)
+    val uri = "influxdb:27017";
+    //val port ="27017"
+    val mongoClient = MongoClient(uri)
+
+    //先删除在创建
+    mongoClient(mongoConfig.db)(MONGODB_MOVIE_COLLECTION).dropCollection()
+    mongoClient(mongoConfig.db)(MONGODB_RATING_COLLECTION).dropCollection()
+    mongoClient(mongoConfig.db)(MONGODB_TAG_COLLECTION ).dropCollection()
+
+    // 将DF写入对应的表中
+    movieDF.write.option("uri",mongoConfig.uri)
+      .option("collection",MONGODB_MOVIE_COLLECTION)
+      .mode("overwrite")
+      .format("com.mongodb.spark.sql")
+      .save()
+
+    ratingDF.write.option("uri",mongoConfig.uri)
+      .option("collection",MONGODB_RATING_COLLECTION)
+      .mode("overwrite")
+      .format("com.mongodb.spark.sql")
+      .save()
+
+    tagDF.write.option("uri",mongoConfig.uri)
+      .option("collection",MONGODB_TAG_COLLECTION)
+      .mode("overwrite")
+      .format("com.mongodb.spark.sql")
+      .save()
+
+    // 建索引
+    mongoClient(mongoConfig.db)(MONGODB_MOVIE_COLLECTION).createIndex(MongoDBObject("mid"->1))
+    mongoClient(mongoConfig.db)(MONGODB_RATING_COLLECTION).createIndex(MongoDBObject("uid"->1))
+    mongoClient(mongoConfig.db)(MONGODB_RATING_COLLECTION).createIndex(MongoDBObject("mid"->1))
+    mongoClient(mongoConfig.db)(MONGODB_TAG_COLLECTION).createIndex(MongoDBObject("uid"->1))
+    mongoClient(mongoConfig.db)(MONGODB_TAG_COLLECTION).createIndex(MongoDBObject("mid"->1))
 
   }
 
-  private def storeDataInES() = ???
+  //private def storeDataInES() = ???
 
 
 
